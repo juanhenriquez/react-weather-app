fad273733f9773ed29a0c68cdc0ae23aff049e3d
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2017-02-05 18:32:39 -0400

    Fix localhost redirects in the server file.

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 8888;
 
 app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`==> Express server is up on port ${PORT}`);
