b2724cea88611134d5d79bc9b7130c1dc5d74c01
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2017-02-05 03:07:15 -0400

    Create the express server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 7000;
+
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`==> Express server is up on port ${PORT}`);
+});
