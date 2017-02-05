2bb8ff0ffa7a42021871201ead04620a0cf54e16
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2017-02-05 02:09:08 -0400

    Create the openWeatherMap service

---
+++ src/api/openWeatherMap.jsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+class OpenWeatherMap {
+  constructor(location) {
+    this.location = location;
+    this.url = "http://api.openweathermap.org/data/2.5/weather?&appid=6231695ffccbb9f81498e370814ce5c6&units=metric";
+  }
+  getTemp() {
+    var requestUrl = this.parseUrl();
+    return axios.get(requestUrl).then(response => {
+      if (response.data.cod &&  response.data.message) {
+        throw new Error(response.data.message);
+      } else {
+        return response.data.main.temp;
+      }
+    }, reason => {
+      throw new Error(reason.data.message);
+    });
+  }
+  parseUrl() {
+    let encodedLocation = encodeURIComponent(this.location);
+    let requestUrl = `${this.url}&q=${encodedLocation}`;
+    return requestUrl;
+  }
+}
+
+export default OpenWeatherMap;
